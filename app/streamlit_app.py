dccc0eaf0e47e33288e1cb7accd5d8352ef63f64
L.JangHyun <zxcz9878@gmail.com>
2024-11-02 01:06:21 +0900

    [FEAT] DB 연동

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,7 +1,25 @@
 import streamlit as st
+import pandas as pd
 
 def main():
     st.title("중고차 매매 데이터")
+
+    table_name = st.secrets["database"]["table_name"]
+    with st.spinner("Loading..."):
+        conn = st.connection("mysql", type="sql")
+
+
+        query = f"""
+        SELECT *
+        FROM `{table_name}`
+        """
+        
+        sold = pd.DataFrame(conn.query(query))
+        sold.index += 1
+    
+    # id 컬럼의 콤마 제거
+    st.dataframe(sold, column_config={"id": st.column_config.NumberColumn(format="%f")},)
+    
 
     with st.sidebar:
         st.sidebar.title("이것은 사이드바")
