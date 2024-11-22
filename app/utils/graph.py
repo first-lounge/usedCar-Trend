7e6c42deb19a578f11d2207df9043aef63416e98
L.JangHyun <zxcz9878@gmail.com>
2024-11-22 15:43:28 +0900

    [ADD] pie와 bar 그래프 작성 코드 추가

---
+++ app/utils/graph.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+def get_brand_bar(brand):    
+    fig = px.bar(brand, y='cnt', x='brand', text_auto=True, title="All Brand")
+    fig.update_xaxes(tickangle=40)
+    return fig
+    
+def get_sold_pie(car):
+    fig =  px.pie(car, values='cnt', names='names', title='Sold Car Top 10')
+    return fig
