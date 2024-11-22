5b3d76f5a17406793194f5866960d2c2ff6fb7b3
L.JangHyun <zxcz9878@gmail.com>
2024-11-23 00:52:03 +0900

    [FIX] pie chart title

--- app/utils/graph.py
+++ app/utils/graph.py
@@ -3,10 +3,10 @@ import streamlit as st
 import plotly.express as px
 
 def get_brand_bar(brand):    
-    fig = px.bar(brand, y='cnt', x='brand', text_auto=True, title="All Brand")
+    fig = px.bar(brand, y='cnt', x='brand', text_auto=True, title="Brand")
     fig.update_xaxes(tickangle=40)
     return fig
     
 def get_sold_pie(car):
-    fig =  px.pie(car, values='cnt', names='names', title='Sold Car Top 10')
+    fig =  px.pie(car, values='cnt', names='names', title='Sold Car')
     return fig
