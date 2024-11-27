004cf1cd3b0479951c5bfdeedbeada4c57fe5dc7
L.JangHyun <zxcz9878@gmail.com>
2024-11-27 16:44:24 +0900

    [FEAT] scatter 함수 추가

--- app/utils/graph.py
+++ app/utils/graph.py
@@ -1,12 +1,14 @@
-import pandas as pd
-import streamlit as st
 import plotly.express as px
 
 def get_brand_bar(brand):    
-    fig = px.bar(brand, y='cnt', x='brand', text_auto=True, title="Brand")
+    fig = px.bar(brand, y='cnt', x='brand', text_auto=True, color="brand", title="Brand")
     fig.update_xaxes(tickangle=40)
     return fig
     
 def get_sold_pie(car):
     fig =  px.pie(car, values='cnt', names='names', title='Sold Car')
+    return fig
+
+def get_not_sold_scatter(car):
+    fig = px.scatter(car, x='price', y='km')
     return fig
