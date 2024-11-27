42356db2ff5fcbd0d935f56f0c4e6433721712f1
L.JangHyun <zxcz9878@gmail.com>
2024-11-27 19:01:21 +0900

    [FIX] 오탈자 수정

--- airflow/daily_dag.py
+++ airflow/daily_dag.py
@@ -17,7 +17,7 @@ default_args = {
     'owner': 'hojae',  # DAG의 소유자 또는 책임자
     'depends_on_past': False,
     'start_date': datetime(2024, 11, 27, tzinfo=local_tz),  # DAG 시작 날짜
-    'catchup':False,
+    'catchup' : False,
     'schedule_interval':timedelta(hours=8), # 8시간마다 실행
     'retries': 4,  # 실패 시 재시도 횟수
     'retry_delay': timedelta(minutes=5),  # 재시도 간 대기 시간
@@ -32,7 +32,7 @@ with DAG(
 ) as dag:
     crawling_task = BashOperator(
         task_id=f'crawling',
-        bash_command=f'python3 {SCRIPT_PATH}\crawling.py'
+        bash_command=f'python3 {SCRIPT_PATH}/crawling.py'
     )
 
     crawling_task
