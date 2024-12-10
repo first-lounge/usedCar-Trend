c450ee0436de137e997a385d76234f63bc20710d
L.JangHyun <zxcz9878@gmail.com>
2024-12-10 22:55:12 +0900

    경로 수정

--- airflow/dag_ex.py
+++ airflow/dag_ex.py
@@ -1,22 +1,26 @@
 from airflow import DAG
+import os
 import datetime
 import pendulum
 from airflow.operators.bash import BashOperator
 
+DIR_PATH = os.path.abspath(__file__)
+SCRIPT_PATH = f'{DIR_PATH}/script'
+
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_ex",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2024, 12, 10, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     bash_t1 = BashOperator(
         task_id="bash_t1",
-        bash_command="echo whoami",
+        bash_command="cat /home/hojae/usedCar-Trend/script/crawling.py",
     )
 
     bash_t2 = BashOperator(
         task_id="bash_t2",
-        bash_command="echo $HOSTNAME",
+        bash_command=f"cd {SCRIPT_PATH}",
     )
 
     bash_t1 >> bash_t2
