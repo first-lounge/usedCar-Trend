843a426eab15495445dd0a2d0827eec52829dae9
L.JangHyun <zxcz9878@gmail.com>
2025-02-19 22:00:43 +0900

    [FIX] get_map_datas 함수 수정

--- app/utils/query.py
+++ app/utils/query.py
@@ -2,6 +2,7 @@ import pymysql
 import pandas as pd
 import streamlit as st
 from geopy.geocoders import Nominatim
+from collections import defaultdict
 
 # 지도에 표시할 나라 설정
 geo_local = Nominatim(user_agent='South Korea')
@@ -173,53 +174,41 @@ def get_map_datas():
     cursor.execute(query)
     lists = cursor.fetchall()
 
-    areas = {'서울':[0], '경기':[0], '인천':[0], '경남':[0],
-                '경북':[0], '전남':[0], '전북':[0], '충남':[0], '충북':[0], '제주':[0], '강원':[0]}
+    areas = defaultdict(int)
 
     for item in lists:
         if item[0] == '전주':
-            areas['전북'].append(item[0])
-            areas['전북'][0] += item[1]
+            areas['전북'] += item[1]
 
         elif item[0] == '청주':
-            areas['충북'].append(item[0])
-            areas['충북'][0] += item[1]
+            areas['충북'] += item[1]
         
         elif item[0] == '원주':
-            areas['강원'].append(item[0])
-            areas['강원'][0] += item[1]
+            areas['강원'] += item[1]
 
         elif item[0] == '제주':
-            areas['제주'].append(item[0])
-            areas['제주'][0] += item[1]
+            areas['제주'] += item[1]
 
         elif item[0] in {'광주수완', '광주풍암'}:
-            areas['전남'].append(item[0])
-            areas['전남'][0] += item[1]
+            areas['전남'] += item[1]
         
         elif item[0] in {'대전', '대전유성', '세종공주', '아산', '천안'}:
-            areas['충남'].append(item[0])
-            areas['충남'][0] += item[1]
+            areas['충남'] += item[1]
         
         elif item[0] in {'경인', '인천'}:
-            areas['인천'].append(item[0])
-            areas['인천'][0] += item[1]
+            areas['인천'] += item[1]
 
         elif item[0] in {'강남', '서초', '영등포', '장한평', '화곡'}:
-            areas['서울'].append(item[0])
-            areas['서울'][0] += item[1]
+            areas['서울'] += item[1]
 
         elif item[0] in {'부산', '서부산', '양산', '울산', '창원마산', '해운대'}:
-            areas['경남'].append(item[0])
-            areas['경남'][0] += item[1]
+            areas['경남'] += item[1]
 
         elif item[0] in {'구미', '대구', '대구반야월', '서대구', '포항'}:
-            areas['경북'].append(item[0])
-            areas['경북'][0] += item[1]
+            areas['경북'] += item[1]
 
         else:
-            areas['경기'].append(item[0])
-            areas['경기'][0] += item[1]
+            areas['경기'] += item[1]
 
     lat, lng, cnts = [], [], []
     names = list(areas.keys())
@@ -227,6 +216,6 @@ def get_map_datas():
     for k in names:
         lat.append(geocoding(k)[0])
         lng.append(geocoding(k)[1])
-        cnts.append(areas[k][0])
+        cnts.append(areas[k])
     
     return lat, lng, names, cnts
