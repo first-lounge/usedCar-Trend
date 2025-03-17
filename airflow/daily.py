3cfe9e83b484a785a79231356f5837474ba78ec7
L.JangHyun <zxcz9878@gmail.com>
2025-03-17 16:35:08 +0900

    [FIX] dag 수정

--- airflow/daily.py
+++ airflow/daily.py
@@ -6,32 +6,28 @@ from airflow.operators.email import EmailOperator
 
 
 
-default_args = {
-    'email_on_retry': False,
-    'email_on_failure': True,
-    'email' : ['zxcz9878@email.com']
-}
-
-
 with DAG(
     dag_id="daily",
-    default_args=default_args,
-    start_date=datetime(2025, 3, 17, tzinfo=pendulum.timezone("Asia/Seoul")),   # 한국 시간 timezone 설정
+    # schedule="0 8 1 * *", # 매월 1일 8시
+    start_date=pendulum.datetime(2025, 3, 17, tzinfo=pendulum.timezone("Asia/Seoul")),
     catchup=False
 ) as dag:
-    send = EmailOperator(
-        task_id='send_email',
-        to='pirouette36@naver.com',
-        subject='[TEST] 테스트 메일입니다.',
-        html_content="""
-                        테스트 메일입니다.<br/><br/> 
-                        ninja template<br/>
-                        {{ data_interval_start }}<br/>
-                        {{ ds }}<br/>
-                    """,
+    send_email = EmailOperator(
+        task_id='email_test',
+        to='pirouette36@naver.com', # 수신자
+        # cc= '참조'
+        subject='Airflow 성공메일', # 제목
+        html_content='Airflow 작업이 완료되었습니다' # 내용
     )
-    
-    send
+
+send_email
+
+
+# with DAG(
+#     dag_id="daily",
+#     start_date=datetime(2025, 3, 17, tzinfo=pendulum.timezone("Asia/Seoul")),   # 한국 시간 timezone 설정
+#     catchup=False
+# ) as dag:
     # crawling = BashOperator(
     #     task_id="start_crawling",
     #     bash_command=f"cd /root/usedCar-Trend/script; python3 crawling.py"
