28e2ce5081416b61fcefda4e9bd400b4c746571a
L.JangHyun <zxcz9878@gmail.com>
2025-03-20 11:56:57 +0900

    [FIX] bash command

--- airflow/daily.py
+++ airflow/daily.py
@@ -20,7 +20,8 @@ with DAG(
 ) as dag:
     crawling = BashOperator(
         task_id="start_crawling",
-        bash_command="cd /root; source proj/bin/activate; python3 usedCar-Trend/script/crawling.py"
+        bash_command= "pwd"
+        # "cd /root; source proj/bin/activate; python3 usedCar-Trend/script/crawling.py"
     )
 
     crawling
